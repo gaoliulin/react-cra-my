5fa2b9fda506e7efaa8593c71dcbdec40c5e93a9
gaoliulin <410157812@qq.com>
2022-10-31 02:14:22 +0800

    add homepage

---
+++ src/home/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './index.css';
+
+
+class HomePage extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    render(){
+        return (
+            <div className="App">
+            <header className="App-header">
+                <h1>
+                homepage2
+                </h1>
+            </header>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
