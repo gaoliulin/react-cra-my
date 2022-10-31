72f6b391857f77a6412b4129d80fee88910df1b3
gaoliulin <410157812@qq.com>
2022-10-31 23:03:58 +0800

    组件复合

--- src/home/index.js
+++ src/home/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './index.css';
 
 import HocFunc from '../components/HOC';
-
+import Header from '../components/Header';
 
 class HomePage extends React.Component {
     constructor(props){
@@ -17,6 +17,9 @@ class HomePage extends React.Component {
             <div className="App">
                 <header className="App-header">
                     <HocFunc text="header"/>
+                    <Header>
+                        <div>组价复合</div>
+                    </Header>
                 </header>
                 <HocFunc text="homepage2"/>
                 <HocFunc text="button"/>
