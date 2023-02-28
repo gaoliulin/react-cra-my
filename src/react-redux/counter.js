8a661e4d846ff908858e336eefff3983c09bbfdd
gaoliulin <410157812@qq.com>
2023-02-28 22:57:14 +0800

    connect(mapStateToProps, mapDispatchToProps)(Counter)

--- src/react-redux/counter.js
+++ src/react-redux/counter.js
@@ -9,15 +9,15 @@ class Counter extends React.Component {
             <>
                 <p>Count：{this.props.count}</p>
                 <br/>
-                <button onClick={this.props.add}>Count++</button>
+                <button onClick={this.props.dispatch(actions.add)}>Count++</button>
                 &nbsp;&nbsp;&nbsp;
-                <button onClick={this.props.minus}>Count--</button>
+                <button onClick={this.props.dispatch(actions.minus)}>Count--</button>
 
                 {/* 这样的时候需要吧mapDispatchToProps去掉，否则把  dispatch map 丢了 */}
-                {/* <br/>
-                <button onClick={ ()=> this.props.dispatch({type: 'ADD'})}>Count++</button>
+                <br/>
+                {/* <button onClick={ ()=> this.props.add()}>Count++</button>
                 &nbsp;&nbsp;&nbsp;
-                <button onClick={ ()=> this.props.dispatch({type: 'MINUS'})}>Count--</button> */}
+                <button onClick={ ()=> this.props.minus()}>Count--</button> */}
             </>
         );
     }
@@ -25,9 +25,19 @@ class Counter extends React.Component {
 
 const mapStateToProps = (state) => state; // 函数，监视store中的state的变化
 
-const mapDispatchToProps = { //对象：保存的action
-    ...actions
+
+ //
+const mapDispatchToProps = (dispatch, ownProps) => { 
+    return {
+        add: ()=>{ 
+            dispatch(actions.add)
+        },
+        minus: ()=>{
+            dispatch(actions.minus)
+        },
+    }
 }
+
 
 // 将store中的state和需要store派发的action绑定到组件中
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
